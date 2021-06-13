6c0cc4be7b5a5e4363e55fec99ed1c556ef7090c
Oscar <oscardawe@googlemail.com>
2021-06-13 21:14:37 +0100

    hover initial commit
    no code written, just making a class and understanding the new cmake

---
+++ take_off/include/hover.hpp
@@ -0,0 +1,12 @@
+#include "ros.hpp"
+
+class hover()
+{
+    private:
+
+    protected:
+    int hover_height = 0.5; // hover height in meters
+
+    public:
+    
+};
---
+++ take_off/src/hover.cpp
@@ -0,0 +1,2 @@
+#include "include/hover.hpp"
+
